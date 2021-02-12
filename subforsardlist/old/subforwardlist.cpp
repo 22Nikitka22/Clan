b649526b65e87760fe2f444a6479935ae8b8af5c
Nikita <bulanov.na@phystech.edu>
2021-02-12 18:13:37 +0300

    Next

--- subforsardlist/old/subforwardlist.cpp
+++ subforsardlist/old/subforwardlist.cpp
@@ -6,104 +6,104 @@ struct subforwardlist {
 };
 
 //Инициализация пустого недосписка
-bool init(subforwardlist** lst)
+bool init(subforwardlist** sfl)
 {
-  assert(lst != nullptr);
-  *lst = nullptr;
+  assert(sfl != nullptr);
+  *sfl = nullptr;
   return true;
 }
 //Инициализация пустого недосписка
 
 //Добавление элемента в конец недосписка
-bool push_back(subforwardlist** lst, int d)
+bool push_back(subforwardlist** sfl, int d)
 {
-  assert(lst != nullptr);
-  if (*lst != nullptr)
-    return push_back(&(*lst)->next, d);
-  *lst = new subforwardlist;
-  (*lst)->data = d;
-  (*lst)->next = nullptr;
+  assert(sfl != nullptr);
+  if (*sfl != nullptr)
+    return push_back(&(*sfl)->next, d);
+  *sfl = new subforwardlist;
+  (*sfl)->data = d;
+  (*sfl)->next = nullptr;
   return true;
 }
 //Добавление элемента в конец недосписка
 
 //Удаление элемента с конца недосписка
-int pop_back(subforwardlist** lst) //удаление элемента с конца недосписка
+int pop_back(subforwardlist** sfl) //удаление элемента с конца недосписка
 {
-  assert(lst != nullptr);
-  if (*lst == nullptr)
+  assert(sfl != nullptr);
+  if (*sfl == nullptr)
     return 0;
-  if ((*lst)->next != nullptr)
-    return pop_back(&(*lst)->next);
-  int wasted = (*lst)->data;
-  delete *lst;
-  *lst = nullptr;
+  if ((*sfl)->next != nullptr)
+    return pop_back(&(*sfl)->next);
+  int wasted = (*sfl)->data;
+  delete *sfl;
+  *sfl = nullptr;
   return wasted;
 }
 //Удаление элемента с конца недосписка
 
 //Добавление элемента в начало недосписка
-bool push_forward(subforwardlist** lst, int d)
+bool push_forward(subforwardlist** sfl, int d)
 {
-  assert(lst != nullptr);
-  subforwardlist* lst2 = new subforwardlist;
-  lst2->data = d;
-  lst2->next = *lst;
-  *lst = lst2;
+  assert(sfl != nullptr);
+  subforwardlist* sfl2 = new subforwardlist;
+    sfl2->data = d;
+    sfl2->next = *sfl;
+  *sfl = sfl2;
   return true;
 }
 //Добавление элемента в начало недосписка
 
 //Удаление элемента из начала недосписка
-int pop_forward(subforwardlist** lst)
+int pop_forward(subforwardlist** sfl)
 {
-  assert(lst != nullptr);
-  if (*lst == nullptr)
+  assert(sfl != nullptr);
+  if (*sfl == nullptr)
     return 0;
-  int wasted = (*lst)->data;
-  subforwardlist* excess= *lst;
-  *lst = (*lst)->next;
+  int wasted = (*sfl)->data;
+  subforwardlist* excess= *sfl;
+  *sfl = (*sfl)->next;
   delete excess;
   return wasted;
 }
 //Удаление элемента из начала недосписка
 
 //Добавление элемента с порядковым номером where
-bool push_where(subforwardlist** lst, unsigned int where, int d)
+bool push_where(subforwardlist** sfl, unsigned int where, int d)
 {
-  assert(lst != nullptr);
+  assert(sfl != nullptr);
   if (where > 1)
-    return push_where(&(*lst)->next, where - 1, d);
-  return push_forward(lst, d);
+    return push_where(&(*sfl)->next, where - 1, d);
+  return push_forward(sfl, d);
 }
 //Добавление элемента с порядковым номером where
 
 //Удаление элемента с порядковым номером where
-bool erase_where(subforwardlist** lst, unsigned int where)
+bool erase_where(subforwardlist** sfl, unsigned int where)
 {
-  assert(lst != nullptr);
+  assert(sfl != nullptr);
   if (where > 1)
-    return erase_where(&(*lst)->next, where - 1);
-  pop_forward(lst);
+    return erase_where(&(*sfl)->next, where - 1);
+  pop_forward(sfl);
   return true;
 }
 //Удаление элемента с порядковым номером where
 
 //Определить размер недосписка
-unsigned int size(subforwardlist** lst)
+unsigned int size(subforwardlist** sfl)
 {
-  assert(lst != nullptr);
-  if ((*lst) == nullptr)
+  assert(sfl != nullptr);
+  if ((*sfl) == nullptr)
     return 0;
-  return (size(&(*lst)->next) + 1);
+  return (size(&(*sfl)->next) + 1);
 }
 //Определить размер недосписка
 
 //Очистить содержимое недосписка
-void clear(subforwardlist** lst)
+void clear(subforwardlist** sfl)
 {
-  unsigned int Size = size(lst);
+  unsigned int Size = size(sfl);
   for (int i = 0; i < Size; ++i)
-    pop_forward(lst);
+    pop_forward(sfl);
 }
 //Очистить содержимое недосписка
